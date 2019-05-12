1057f8dd1f628de8563bfecc76354d3619b01bce
Robert Sweetman <robertiansweetman@gmail.com>
2019-05-12 18:43:16 +0100

    Connect to vSpere

--- ElasticsearchSetup.tf
+++ ElasticsearchSetup.tf
@@ -19,3 +19,21 @@ provider "vsphere" {
 
   allow_unverified_ssl = true
 }
+
+data "vsphere_datacenter" "datacenter" {
+  name = "${module.credentials.datacenter_name}"
+}
+
+output "datacenter_id" {
+  value = "${data.vsphere_datacenter.datacenter.id}"
+}
+
+data "vsphere_network" "network" {
+  name          = "${module.credentials.network_name}" # Default is "VM Network"
+  datacenter_id = "${data.vsphere_datacenter.datacenter.id}"
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = "${module.credentials.resource_pool_name}" # ESXi can leave this blank
+  datacenter_id = "${data.vsphere_datacenter.datacenter.id}"
+}
